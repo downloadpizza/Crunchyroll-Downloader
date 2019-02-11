11af973a791bb694380ae533dd63d911f8e41eef
Gabgab2003 <klenngabriel@gmail.com>
2019-02-11 11:36:50 +0100

    Useful info in output

--- src/crunchy.js
+++ src/crunchy.js
@@ -1,45 +1,56 @@
 const request = require("request");
 const { spawn } = require('child_process');
 
+main();
 
-const options = {
-    url: "https://www.crunchyroll.com/en-gb/a-certain-magical-index/episode-14-heroes-781240",
-    headers: {}
-};
-
-request.get(options, function (error, response, body) {
-    if(error) {
-        return console.log(error);
+function main() {
+    if(process.argv.length < 3) {
+        console.log('Usage: crunchy [url]');
+    } else {
+        download(process.argv[2]);
     }
-    const start = "vilos.config.media = ";
-    const end = "vilos.config.analytics = ";
-    console.log(body.search(start))
-    console.log(body.search(end))
-    const test = body.substring(body.search(start)+start.length, body.search(end)-7);
-    let langs = {}
-    const meta = JSON.parse(test);
-    const streams = meta.streams;
-    for(let i in streams) {
-        langs[streams[i].hardsub_lang] = streams[i].url
-    }
-    console.log(langs["enUS"]);
-    getStream(langs["enUS"])
-})
-
-function getStream(url) {
-    let ffmpeg = spawn("ffmpeg", ['-i', url, '-c','copy', 'live.mkv']);
-    ffmpeg.stdout.on('data', (data) => {
-        console.log(data);
-    });
-
-    ffmpeg.stderr.on('data', (data) => {
-        console.log(data);
-    });
-
-    ffmpeg.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-    });
 }
 
+function download(url) {
+    const options = {
+        url: url,
+        headers: {}
+    };
+    const outname = url.replace(/https?:\/\/.+\..+\/[a-z]+-[a-z]+\//, "").replace(/[^a-zA-Z0-9]/g, "_")+'.mkv';
+    console.log(outname);
 
+    request.get(options, function (error, response, body) {
+        if (error) {
+            return console.log(error);
+        }
+        const start = "vilos.config.media = ";
+        const end = "vilos.config.analytics = ";
+        if(body.search(start)==-1 || body.search(end)==-1) {
+            console.log('error');
+            return
+        }
+        const test = body.substring(body.search(start) + start.length, body.search(end) - 7);
+        let langs = {};
+        const meta = JSON.parse(test);
+        const streams = meta.streams;
+        for (let i in streams) {
+            langs[streams[i].hardsub_lang] = streams[i].url
+        }
+        getStream(langs["enUS"])
+    });
 
+    function getStream(url) {
+        let ffmpeg = spawn("ffmpeg", ['-hide_banner', '-v', 'quiet', '-stats', '-i', url, '-c', 'copy', outname]);
+        ffmpeg.stdout.on('data', (data) => {
+            console.log(`${data}`);
+        });
+
+        ffmpeg.stderr.on('data', (data) => {
+            console.log(`${data}`);
+        });
+
+        ffmpeg.on('close', (code) => {
+            console.log(`ffmpeg exited with code ${code}`);
+        });
+    }
+}
