fb236de8343f915aef7f0239233bd8d54d92d3c7
Gabgab2003 <klenngabriel@gmail.com>
2019-02-11 08:01:06 +0100

    Downloader Works, removed cookie

--- src/crunchy.js
+++ src/crunchy.js
@@ -1,26 +1,45 @@
 const request = require("request");
-const fs = require("fs");
+const { spawn } = require('child_process');
+
 
 const options = {
     url: "https://www.crunchyroll.com/en-gb/a-certain-magical-index/episode-14-heroes-781240",
-    headers: {
-        Cookie: "_ga=GA1.2.1307886796.1549315073; _fbp=fb.1.1549315073373.549583704; ajs_group_id=null; __qca=P0-1748109036-1549315074373; ajs_anonymous_id=%22d59bde3e-f8b0-4d62-b147-1b37fde30fb8%22; ajs_user_id=%2287292366%22; _gid=GA1.2.414058716.1549729040; amplitude_idundefinedcrunchyroll.com=eyJvcHRPdXQiOmZhbHNlLCJzZXNzaW9uSWQiOm51bGwsImxhc3RFdmVudFRpbWUiOm51bGwsImV2ZW50SWQiOjAsImlkZW50aWZ5SWQiOjAsInNlcXVlbmNlTnVtYmVyIjowfQ==; amplitude_id_f92f25db4ed7f1c7ee55351799d43351crunchyroll.com=eyJkZXZpY2VJZCI6ImMwNDZiMTA4LWYyMTEtNGRjZC1hMDA5LTY3YjUyYjRmMDYxNVIiLCJ1c2VySWQiOiI4NzI5MjM2NiIsIm9wdE91dCI6ZmFsc2UsInNlc3Npb25JZCI6MTU0OTgyMjAxMDg2NywibGFzdEV2ZW50VGltZSI6MTU0OTgyMjM0ODUzMywiZXZlbnRJZCI6MzQsImlkZW50aWZ5SWQiOjQ0LCJzZXF1ZW5jZU51bWJlciI6Nzh9"
-    }
-}
+    headers: {}
+};
 
 request.get(options, function (error, response, body) {
     if(error) {
         return console.log(error);
     }
-    const start = "vilos.config.media = "
-    const end = "vilos.config.analytics = "
+    const start = "vilos.config.media = ";
+    const end = "vilos.config.analytics = ";
+    console.log(body.search(start))
+    console.log(body.search(end))
     const test = body.substring(body.search(start)+start.length, body.search(end)-7);
     let langs = {}
-    const streams = JSON.parse(test).streams;
+    const meta = JSON.parse(test);
+    const streams = meta.streams;
     for(let i in streams) {
         langs[streams[i].hardsub_lang] = streams[i].url
     }
-    let req = request(langs["enUS"])
-    req.on("response")
+    console.log(langs["enUS"]);
+    getStream(langs["enUS"])
+})
+
+function getStream(url) {
+    let ffmpeg = spawn("ffmpeg", ['-i', url, '-c','copy', 'live.mkv']);
+    ffmpeg.stdout.on('data', (data) => {
+        console.log(data);
+    });
+
+    ffmpeg.stderr.on('data', (data) => {
+        console.log(data);
+    });
+
+    ffmpeg.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
 }
-)
+
+
+
